52d9d30d26a5bf8ba5b635536d785331c428016f
wingedseraph <r89647@gmail.com>
2024-08-11 18:12:21 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  // reverse(123); // 321
+  // reverse(233); // 332
+  // reverse(535); // 535
+  // reverse(95034); // 43059
+
+  /*
+  нужно использовать method reverse
+  -> это array method
+  выходит мне надо сначала сделать number -> string -> array
+  использовать array.reverse()
+  вернуть array -> number
+  return number
+  */
+
+  return String(n)
+    .replace(/-/g, "")
+    .split("")
+    .reverse()
+    .toString()
+    .replace(/,/g, "");
+};
